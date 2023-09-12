8968fcad1f2be066ab6330752f51349d9ecf2a85
Kobe <danrok19@wp.pl>
2023-09-12 16:31:41 +0200

    changing form in festival creator

--- src/Components/FormOrganizeFirst/FormOrganizeFirst.jsx
+++ src/Components/FormOrganizeFirst/FormOrganizeFirst.jsx
@@ -5,149 +5,97 @@ import { TfiLocationPin } from 'react-icons/tfi';
 import { HiOutlineSignal } from 'react-icons/hi2';
 import { FaCalendarAlt } from 'react-icons/fa';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 
 const FormOrganizeFirst = () => {
 
-    const [pickOnline, setPickOnline] = useState(false);
-    const [day, setDay] = useState('');
-    const [isCheckedClock, setIsCheckedClock] = useState(false);
-    const [isCheckedEndDate, setIsCheckedEndDate] = useState(false);
+    const [isOnline, setIsOnline] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
 
-    useEffect(() => {
-        const currentDate = new Date();
-        if (currentDate.getMonth() + 1 <= 11) {
-            setDay(currentDate.getDate() + '.0' + (currentDate.getMonth() + 1) % 12 + '.' + currentDate.getFullYear()); // Months are zero-indexed, so we add 1
-        }
-        else {
-            setDay(currentDate.getDate() + '.' + (currentDate.getMonth() + 1) % 12 + '.' + currentDate.getFullYear());
-        }
-    }, []);
+    const setNotOnline = () => {
+        setIsOnline(false);
 
-    const changeToOnline = () => {
-        setPickOnline(true);
     }
 
-    const changeToOffline = () => {
-        setPickOnline(false);
+    const setOnline = () => {
+        setIsOnline(true);
+
     }
 
-    const handleCheckboxClock = (event) => {
-        setIsCheckedClock(event.target.checked);
-    }
 
-    const handleCheckboxEndDate = (event) => {
-        setIsCheckedEndDate(event.target.checked);
-    }
 
     return (
-        <div>
-            <div className='firstSection'>
-                <div className='headLine'>
+        <form className="form-template">
+            <div>
+                <div className="data-section">
                     <h1>Informacje tytułowe</h1>
-                    <hr class="line"></hr>
-                </div>
-                <div className='inputs'>
-                    <div className='inputSection'>
-                        <label>Tytuł festiwalu</label>
-                        <input type="text" placeholder='Prosty i treściwy tytuł' />
+                    <hr class="line" />
+                    <div class="input-template">
+                        <label for="title">Tytuł festiwalu </label>
+                        <input type="text" name="title" id="title" required />
                     </div>
-                    <div className='inputSection'>
-                        <label>Organizatorzy</label>
-                        <input type="text" placeholder='Kto organizuje ten festiwal' />
+                    <div class="input-template">
+                        <label for="organizer">Organizatorzy </label>
+                        <input type="text" name="organizer" id="organizer" required />
                     </div>
                 </div>
-            </div>
-            <div className='secondSection'>
-                <div className='headLine'>
-                    <h1>Lokalizacja</h1>
-                    <hr class="line"></hr>
-                </div>
+                <h1>Lokalizacja</h1>
+                <hr class="line" />
                 <div className="buttons">
-                    <Button secondary onClick={changeToOffline}>Miejsce <TfiLocationPin /></Button>
-                    <Button secondary onClick={changeToOnline}>Online <HiOutlineSignal /></Button>
+                    <Button primary onClick={setNotOnline}>Miejsce</Button>
+                    <Button primary onClick={setOnline}>Online</Button>
                 </div>
-                <div className='inputs'>
-                    {pickOnline ?
-                        <div className='inputSection'>
-                            <label>Informacje o lokalizacji wydarzeń</label>
-                            <input type="text" placeholder='Podaj lokalizacje' />
+                {isOnline ?
+                    <div className="online">
+                        <div class="input-template">
+                            <label for="information">Informacje na temat miejsca spotkania</label>
+                            <input type="text" name="information" id="information" required />
                         </div>
-                        :
-                        <>
-                            <div className='inputSection'>
-                                <label>Adres festiwalu</label>
-                                <input type="text" placeholder='Podaj adres wydarzenia' />
-                            </div>
-                            <div className='inputSection'>
-                                <label>Dodatkowe informacje</label>
-                                <input type="text" placeholder='Podaj dodatkowe informacje o lokalizacji' />
-                            </div>
-                        </>
-                    }
-                </div>
-            </div>
-            <div className='thirdSection'>
-                <div className='headLine'>
-                    <h1>Data i czas</h1>
-                    <hr class="line"></hr>
-                </div>
-                <div className="inputs">
-                    <div className='inputSection'>
-                        <label>Początek festiwalu</label>
-                        <input type="text" placeholder={day} />
-                        <FaCalendarAlt className='icons' />
                     </div>
-                    {isCheckedClock ?
-                        <div className='inputSection'>
-                            <label>Godzina rozpoczęcia</label>
-                            <input type="text" placeholder='12.00' />
+                    :
+                    <div className="not-online">
+                        <div class="input-template">
+                            <label for="address">Adres festiwalu </label>
+                            <input type="text" name="address" id="address" required />
                         </div>
-                        :
-                        <></>}
+                        <div class="input-template">
+                            <label for="additionalInformation">Dodatkowe informacje </label>
+                            <input type="text" name="additionalInformation" id="additionalInformation" required />
+                        </div>
+                    </div>}
+                <h1>Data i czas</h1>
+                <hr class="line" />
+                <div className="date-inputs">
+                    <div className="date-picker">
+                        <label>Początek imprezy</label>
+                        <DatePicker
+                            showIcon
+                            dateFormat="dd/MM/yyyy"
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            isClearable
+                            placeholderText="Wybierz nową datę!"
+                        />
+                    </div>
+                    <div className="date-picker">
+                        <label>Koniec imprezy</label>
+                        <DatePicker
+                            showIcon
+                            dateFormat="dd/MM/yyyy"
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            isClearable
+                            placeholderText="Wybierz nową datę!"
+                        />
+                    </div>
+                </div>
+                <div className="button-section">
+                    <Button secondary className="button-next">Dalej</Button>
+                </div>
 
-                    <>
-                        <label style={{ paddingTop: '15px', paddingRight: '55px', display: 'inline-block', whiteSpace: 'nowrap' }}>
-                            <input
-                                type="checkbox"
-                                checked={isCheckedClock}
-                                onChange={handleCheckboxClock}
-                            />
-                            Pokaż godzinę
-                        </label></>
-                </div>
-                <div className="inputs">
-                    {isCheckedEndDate ?
-                        <>
-                            <div className='inputSection'>
-                                <label>Koniec festiwalu</label>
-                                <input type="text" placeholder={day} />
-                                <FaCalendarAlt className='icons' />
-                            </div>
-                            {isCheckedClock ?
-                                <div className='inputSection'>
-                                    <label>Godzina zakończenia</label>
-                                    <input type="text" placeholder='12.00' />
-                                </div>
-                                :
-                                <></>
-                            }
-                        </>
-                        :
-                        <></>}
-                    <>
-                        <label style={{ paddingTop: '15px', display: 'inline-block', whiteSpace: 'nowrap'}}>
-                            <input
-                                type="checkbox"
-                                checked={isCheckedEndDate}
-                                onChange={handleCheckboxEndDate}
-                            />
-                            Pokaż datę zakończenia
-                        </label>
-                    </>
-                </div>
             </div>
-            <Button primary className='submit'>Dalej <BsFillArrowRightCircleFill /></Button>
-        </div>
+        </form>
     )
 }
 
