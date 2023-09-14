ce4abacb8f89f41b7f1274ba28180110c2882245
Kobe <danrok19@wp.pl>
2023-09-14 15:08:03 +0200

    begin the eventPage

--- src/Pages/Event/EventPage.jsx
+++ src/Pages/Event/EventPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './eventPage.css';
 
 const EventPage = () => {
 
-    return(
-        <div>
-            Siema
+    return (
+        <div className="event-section">
+            <div className="image-section">
+                <img className="blured-img" src='https://picsum.photos/1920/700' alt="Zdjęcie" />
+                <div className="image-wrapper">
+                    <img src='https://picsum.photos/1920/700' alt="Zdjęcie" />
+
+                    <div className="info-wrapper">
+                        <p>Nazwa Organizatora</p>
+                        <p>Nazwa Wydarzenia</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
