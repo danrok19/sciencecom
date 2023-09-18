0e2fd3bbb4f26c5385fe71e0eddf31008f803239
Kobe <danrok19@wp.pl>
2023-09-18 17:01:45 +0200

    progress in eventPage

--- src/Pages/Event/EventPage.jsx
+++ src/Pages/Event/EventPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import './eventPage.css';
+import Button from '../../Components/Button/Button';
 
 const EventPage = () => {
+
+    const sections = [
+        { label: 'Opis' },
+        { label: 'Daty' },
+        { label: 'Lokalizacja' }
+    ];
+
+
+    const availableSections = sections.map((section) => {
+        return <div key={section.label} className='nav'>{section.label}</div>
+    })
 
     return (
         <div className="event-section">
@@ -11,9 +23,22 @@ const EventPage = () => {
                     <img src='https://picsum.photos/1920/700' alt="Zdjęcie" />
 
                     <div className="info-wrapper">
-                        <p>Nazwa Organizatora</p>
-                        <p>Nazwa Wydarzenia</p>
+                        <p className="organization-name">Nazwa Organizatora</p>
+                        <p className="event-name">Nazwa Wydarzenia</p>
                     </div>
+                </div>
+            </div>
+            <div className="navigation-section">
+                <div className="section-wrapper">
+                    {availableSections}
+                </div>
+            </div>
+            <div className="content-section">
+                <div className="join-section">
+                    <Button primary className="btn">Dołącz</Button>
+                </div>
+                <div className="description-section">
+                    hfuisadhsadsa
                 </div>
             </div>
         </div>
