d5579238224854307b1eff1a1c60f3693d4373a9
Kobe <danrok19@wp.pl>
2023-09-28 14:25:30 +0200

    added changing form

---
+++ src/Components/ChangeEvent/ChangeEvent.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './changeEvent.css';
+import Button from '../Button/Button';
+import { RxCross2 } from 'react-icons/rx';
+import { BsExclamationDiamondFill } from 'react-icons/bs';
+
+const ChangeEvent = () => {
+
+  const [ showWarning, setShowWarning ] = useState(false);
+
+  const onClosing = () =>{
+    setShowWarning(true);
+  }
+
+  return (
+    <div className={showWarning ? 'section-closed' : 'section-wrapper'}>
+        <div className="cross-sign">
+            <h3>Podpowiedź</h3>
+            <RxCross2 style={{cursor: 'pointer'}} onClick={onClosing} />
+        </div>
+        <div className="text-wrapper">
+            <span>Poniższy formularz pozwoli Ci utworzyć festiwal, który musi składać sięz conajmniej dwóch wydarzeń. 
+                Jeżeli chcesz utworzyć jedynie pojedyńcze wydarzenie to przejdź do zaklładki z formularzem tworzenia wydarzenia. <BsExclamationDiamondFill style={{color: '#FFA844', fontSize: '24px'}}/>
+            </span>
+            <Button primary className="btn-create">Utwórz wydarzenie</Button>
+        </div>
+    </div>
+  )
+}
+
+export default ChangeEvent
--- src/Pages/OrganizePage/OrganizePage.jsx
+++ src/Pages/OrganizePage/OrganizePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import FormOrganizeFirst from '../../Components/FormOrganizeFirst/FormOrganizeFirst'
+import ChangeEvent from '../../Components/ChangeEvent/ChangeEvent'
 
 const OrganizePage = () => {
   return (
     <div style={{background: '#950740', paddingTop: '2em', paddingBottom: '3rem'}}>
+      <ChangeEvent />
       <FormOrganizeFirst />
     </div>
   )
