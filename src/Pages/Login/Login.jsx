c84820c2d8f68b11313021d4d6114efee4466268
Kobe <danrok19@wp.pl>
2023-11-18 21:54:22 +0100

    video background in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import LoginBox from '../../Components/LoginBox/LoginBox'
 import RegisterBox from '../../Components/RegisterBox/RegisterBox';
+import video from '../../Assets/video.mp4';
 
 const Login = () => {
 
@@ -14,12 +15,16 @@ const Login = () => {
 
 
   return (
-    <div style={{ minHeight: '91vh', background: '#950740' }}>
+    <div style={{ minHeight: '100vh', background: '#950740', position: 'relative'}}>
+      <div style={{position: 'absolute', zIndex: '1', background: '#850707', mixBlendMode: 'hard-light', height: '100%', width: '100%', top: '0', bottom: '0' }}/>
+      <video src={video} muted autoPlay loop type="video/mp4" style={{position: 'absolute', zIndex: '0',  height: '100%', width: '100%', top: '0', bottom: '-5%', objectFit: 'cover'}}/>
+      <div style={{position: 'relative', zIndex: '2'}}>
       {registerContent ?
         <RegisterBox onSwitchToLogin={onSwitchToLogin} />
         :
         <LoginBox onSwitchToRegister={onSwitchToRegister} />
       }
+      </div>
     </div>
   )
 }
